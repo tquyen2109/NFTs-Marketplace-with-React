cdc19d9fad974413b0f0cccc7f8e1913714760ea
tquyen2109 <t.quyen1997@yahoo.com>
2023-01-17 14:45:42 +0700

    balance of + owner of functions

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -16,12 +16,13 @@ contract ERC721 {
     //Mapping from owner to number of owned token
     mapping(address => uint256) private _OwnedTokensCount;
 
-    function _exists(uint256 tokenId) internal view returns(bool) {
+    function _exists(uint256 tokenId) internal view returns (bool) {
         //setting the address nft owner to check the mapping of the address from tokenOwner at the tokenId
         address owner = _tokenOwner[tokenId];
         //return truthiness that address is not zero
         return owner != address(0);
     }
+
     function _mint(address to, uint256 tokenId) internal {
         //requires that the address is not zero
         require(to != address(0), 'ERC7621: minting to the zero address');
@@ -32,5 +33,15 @@ contract ERC721 {
         //keeping track of each address that is minting and adding one to the count
         _OwnedTokensCount[to] = _OwnedTokensCount[to] + 1;
         emit Transfer(address(0), to, tokenId);
+    }
+
+    function balanceOf(address owner) public view returns (uint256) {
+        require(owner != address(0), 'owner query for non-existing address');
+        return _OwnedTokensCount[owner];
+    }
+
+    function ownerOf(uint256 tokenId) public view returns (address) {
+        require(_exists(tokenId), 'token does not exists  ');
+        return _tokenOwner[tokenId];
     }
 }
