37853dccf5b971998e9faa2d6d494f9b3c49e18b
tquyen2109 <t.quyen1997@yahoo.com>
2023-01-21 20:32:39 +0700

    ERC721 Enumerable update

--- src/contracts/ERC721Enumerable.sol
+++ src/contracts/ERC721Enumerable.sol
@@ -11,24 +11,39 @@ contract ERC721Enumerable is ERC721 {
     mapping(address => uint256[]) private _ownedTokens;
     //mapping from token ID index of te owner tokens list
     mapping(uint256 => uint256) private _ownedTokensIndex;
+    //return total supply of the _allTokens array
     function totalSupply() external view returns (uint256) {
         return _allTokens.length;
     }
-//    function tokenByIndex(uint256 _index) external view returns (uint256) {
-//
-//    }
-//    function tokenOfOwnerByIndex(address _owner, uint256 _index) external view returns (uint256) {
-//
-//    }
+
+    function tokenByIndex(uint256 index) external view returns (uint256) {
+        require(index < totalSupply(), 'global index is out of bound');
+        return _allTokens[index];
+    }
+
+    function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256) {
+        return _ownedTokens[owner][index];
+    }
 
     function _mint(address to, uint256 tokenId) internal override(ERC721) {
         super._mint(to, tokenId);
         //add token to the owner
         //add token to totalSupply
-        _addTokensToTotalSupply(tokenId);
+        _addTokensToAllTokenEnumeration(tokenId);
+        _addTokenToOwnerEnumeration(tokenId, to);
     }
 
-    function _addTokensToTotalSupply(uint256 tokenId) private {
+    //add tokens to the _allTokens array and set the position of the tokens indexes
+    function _addTokensToAllTokenEnumeration(uint256 tokenId) private {
+        _allTokensIndex[tokenId] = _allTokens.length;
         _allTokens.push(tokenId);
+    }
+
+    function _addTokenToOwnerEnumeration(uint256 tokenId, address to) {
+        //add address and tokenId to the _ownedTokens
+        //_ownedTokensIndex tokenId set to the address of ownedTokens position
+        //execute the function with minting
+        _ownedTokensIndex[tokenId] = _ownedTokens[to].length;
+        _ownedTokens[to].push(tokenId);
     }
 }
