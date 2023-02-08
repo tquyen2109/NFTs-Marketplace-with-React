235c8d41d625d5294cc203722a7c8d52b6e2509c
tquyen2109 <t.quyen1997@yahoo.com>
2023-02-08 17:23:43 +0700

    Front end contract + wallet details

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,98 @@
 import React, {Component} from 'react';
-import Web3 from "web3";
 import detectEthereumProvider from "@metamask/detect-provider";
 import KryptoBird from '../abis/KryptoBird.json';
+import { ethers } from "ethers";
 export default class App extends Component {
     async componentDidMount() {
-        await this.loadWeb3();
-        await this.loadBlockchainData();
-    }
-    async loadWeb3 () {
-        const provider = await detectEthereumProvider();
-        if(provider) {
-            console.log('ethereum wallet is connected');
-            window.web3 = new Web3(provider);
-        } else {
-            console.log('no ethereum wallet detected')
-        }
+        await this.loadWeb3ProviderAndBlockchainData();
     }
 
-    async loadBlockchainData() {
-        const account = await window.web3.eth.getAccounts();
-        console.log(account);
+
+    async loadWeb3ProviderAndBlockchainData() {
+
+        // Loads MetaMask as the provider
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+
+        // Check if provider loaded
+        if(provider) {
+        } else {
+            window.alert('No Ethereum Wallet detected');
+        };
+
+        // Get accounts and networkId
+        const accounts = await provider.send('eth_accounts', []);
+        const networkId = await provider.send('net_version', []);
+
+        // Update State with info
+        this.setState({
+            account: accounts,
+            networkIs: networkId
+        });
+
+        // Loads Contract information from the Blockchain
+        const networkData = KryptoBird.networks[networkId];
+
+        if(networkData) {
+
+            const contractAbi = KryptoBird.abi;
+            const contractAddress = networkData.address;
+
+            // Ethers use a segregated approach to reading / writing to Contracts
+            // To read from, use contractRead
+            // To write to (like to Mint, etc, use contractSign
+
+            const contractRead = new ethers.Contract(contractAddress, contractAbi, provider);
+            const contractSign = new ethers.Contract(contractAddress, contractAbi, signer);
+
+            this.setState({
+                contractRead: contractRead,
+                contractSign: contractSign
+            });
+
+        } else {
+            window.alert('Smart Contract is not Deployed')
+        };
+
+        // When calling functions using ethers.js, you can call them directly
+        // The below example calls totalSupply, directly under the contractRead object
+
+        let getTotalSupply = await this.state.contractRead.totalSupply();
+        getTotalSupply = ethers.utils.formatUnits(getTotalSupply, 0);
+
+        this.setState({
+            totalSupply: getTotalSupply
+        });
+
+        for(let i = 0; i < getTotalSupply; i++) {
+
+            const KryptoBird = await this.state.contractRead.kryptoBirdz(i - 0);
+
+            this.setState({
+                kryptoBirdz:[...this.state.kryptoBirdz, KryptoBird]
+            });
+        }
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            account: ''
+        }
     }
     render() {
         return (
             <div>
+                <nav className={'navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow'}>
+                    <div className={'navbar-brand col-sm-3 col-md-3 mr-0'} style={{color: "white"}}>
+                        Krypto Birdz NFTs (Non Fungible Token)
+                    </div>
+                    <ul className={'navbar-nav px-3'}>
+                        <li className={'nav-item text-nowrap d-none d-sm-none d-sm-block'}>
+                            <small className={'text-white'}>{this.state.account}</small>
+                        </li>
+                    </ul>
+                </nav>
                 <h1>NFT Marketplace</h1>
             </div>
         )
